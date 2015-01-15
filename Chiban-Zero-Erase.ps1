69ba197c3dd7b39b110929ff7241a6090629bb6a
Hiroyuki Kimura <z_kimura@k-side.net>
2015-01-15 09:41:00 +0900

    枝番が無かった時にエラーが出たので枝番の Trim Start を if 分岐の後にした

--- Chiban-Zero-Erase.ps1
+++ Chiban-Zero-Erase.ps1
@@ -1,8 +1,8 @@
 function Chiban-Zero-Erase {
    $Chiban = [string]$ARGS[0] -Split "-"
    $Chiban[0] = $Chiban[0].TrimStart("0")
-   $Chiban[1] = $Chiban[1].TrimStart("0")
    if ( $Chiban[1].Length -ne 0 ) {
+      $Chiban[1] = $Chiban[1].TrimStart("0")
       Write-Output $($Chiban[0] + "-" + $Chiban[1])
    } else {
       Write-Output $Chiban[0]
